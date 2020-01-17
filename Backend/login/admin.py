bff9e68c15acb44167df84ab2ac3403c35b81f07
vijeyanidhi <vijeyanidhi.17je003030@am.ism.ac.in>
2020-01-17 21:07:27 +0530

    logindata

--- Backend/login/admin.py
+++ Backend/login/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class LoginDataAdmin(admin.ModelAdmin):
+    list_display = ["ident","password","otp","flag","modified","created"]
+    search_fields = ["ident","password","modified","created"]
+
+admin.site.register(LoginData, LoginDataAdmin)
+
--- Backend/login/models.py
+++ Backend/login/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class LoginData(models.Model):
+    ident = models.CharField(max_length=240, blank=False, null=False)
+    password = models.CharField(max_length=240, blank=False, null=False)
+    otp = models.IntegerField(default=0)
+    flag = models.BooleanField(default=False)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
