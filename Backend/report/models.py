7aa58f73be60e154d5b49238ab4cf2621a0377df
vijeyanidhi <vijeyanidhi.17je003030@am.ism.ac.in>
2020-01-18 09:07:16 +0530

    Sat

--- Backend/report/models.py
+++ Backend/report/models.py
@@ -2,5 +2,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from login.models import *
 
 # Create your models here.
+
+class TermData(models.Model):
+    termName = models.CharField(max_length=240, blank=False, null=True)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __unicode__(self):
+        return str(self.termName)
+
+class Report(models.Model):
+    reportID = models.CharField(max_length=240, blank=False, null=True)
+    customerIdent = models.ForeignKey(LoginData)
+    doctorName = models.CharField(max_length=240, blank=False, null=True)
+    doctorIdent = models.CharField(LoginData)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+class ReportContent(models.Model):
+    termName = models.ForeignKey(TermData, null=True)
+    value = models.CharField(max_length=240, blank=False, null=True)
+    units = models.CharField(max_length=240, blank=False, null=True)
+    refValue = models.CharField(max_length=240, blank = False, null=True)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
--- Backend/report/views.py
+++ Backend/report/views.py
@@ -3,4 +3,43 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
+
 # Create your views here.
+
+@csrf_exempt
+def AddTermName(request):
+    response_json = {}
+    if request.method == 'POST':
+        for x, y in request.POST.items():
+            print("key,value", x, ":", y)
+        vals = request.POST.getlist('vals')
+        for x in vals:
+            if not TermData.objects.filter(termName=x).exists():
+                TermData.objects.create(termName=x)
+        response_json['success'] = True
+        response_json['message'] = 'Successful'
+    else:
+        response_json['success'] = False
+        response_json['message'] = "Not Post Method"
+
+    print (str(response_json))
+    return JsonResponse(response_json)
+
+def AddReport(request):
+    response_json = {}
+    if request.method == 'POST':
+        for x, y in request.POST.items():
+            print("key,value", x, ":", y)
+
+        response_json['success'] = True
+        response_json['message'] = 'Successful'
+    else:
+        response_json['success'] = False
+        response_json['message'] = "Not Post Method"
+
+    print (str(response_json))
+    return JsonResponse(response_json)
+
